82bb66ca6ea7af1a34c6fbf3393cc35f8a14b047
Juan Esteban Torres Pachon <esteban67pachon@outlook.com>
2024-08-30 17:42:45 -0500

    Average / Students

--- Trabajo_Promedio/average.py
+++ Trabajo_Promedio/average.py
@@ -1,10 +1,17 @@
+#Se Solicita el ingreso de las cuatro notas.
 c1 = int(input("1: "))
 c2 = int(input("2: "))
 c3 = int(input("3: "))
 c4 = int(input("4: "))
 
+#Se suman las variables anteriores y se almacenan
+#En la variable s.
 s = c1 + c2 + c3 + c4
+
+#Se divide la variable s entre el número total
+#De datos.
 p = s / 4
 
+#Se imprime la salida.
 print(f"Promedio: {p}")
 
